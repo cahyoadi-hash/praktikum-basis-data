bb92cefa6a3aed092121fc47f1ba3f51af110ddc
cahyoadi-hash <cahyoadi@students.untidar.ac.id>
2026-04-26 21:01:16 +0700

    Add files via upload

---
+++ UjiResponsi_2505060034.sql
@@ -0,0 +1,104 @@
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: rental_ps_2505060034
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pelanggan`
+--
+
+DROP TABLE IF EXISTS `pelanggan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` int(3) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `no_hp` int(12) NOT NULL,
+  PRIMARY KEY (`id_pelanggan`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+LOCK TABLES `pelanggan` WRITE;
+/*!40000 ALTER TABLE `pelanggan` DISABLE KEYS */;
+INSERT INTO `pelanggan` VALUES (1,'roni',2147483647),(2,'doni',2147483647),(3,'toni',2147483647),(4,'soni',2147483647),(5,'joni',2147483647);
+/*!40000 ALTER TABLE `pelanggan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ps`
+--
+
+DROP TABLE IF EXISTS `ps`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ps` (
+  `id_ps` int(4) NOT NULL,
+  `jenis_ps` varchar(1) NOT NULL,
+  `harga_per_jam` int(4) NOT NULL,
+  PRIMARY KEY (`id_ps`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ps`
+--
+
+LOCK TABLES `ps` WRITE;
+/*!40000 ALTER TABLE `ps` DISABLE KEYS */;
+INSERT INTO `ps` VALUES (1001,'1',2000),(1002,'2',3000),(1003,'3',4000),(1004,'4',7000),(1005,'5',9000);
+/*!40000 ALTER TABLE `ps` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaksi`
+--
+
+DROP TABLE IF EXISTS `transaksi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(5) NOT NULL,
+  `id_pelanggan` int(3) NOT NULL,
+  `id_ps` int(4) NOT NULL,
+  `lama_sewa_dalam_jam` int(2) NOT NULL,
+  `total_bayar` int(6) NOT NULL,
+  PRIMARY KEY (`id_transaksi`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+LOCK TABLES `transaksi` WRITE;
+/*!40000 ALTER TABLE `transaksi` DISABLE KEYS */;
+INSERT INTO `transaksi` VALUES (11001,2,1003,3,12000),(11003,4,1005,2,14000),(11004,5,1003,4,16000),(11005,3,1002,1,3000);
+/*!40000 ALTER TABLE `transaksi` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-08 11:36:56
